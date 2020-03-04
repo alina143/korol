fce0de2723fa5873bfec207d3968ff1090286e6b
alina143 <52449274+alina143@users.noreply.github.com>
2020-03-04 15:36:19 +0700

    Update root.py

--- root.py
+++ root.py
@@ -24,32 +24,32 @@ def suchestv_viborka():
         if m < g(r):
             return r
 
-c = ROOT.TCanvas("myCanvasName", "The Canvas Title", 1200, 500)
+c = ROOT.TCanvas("myfunc", "The Canvas Title", 1200, 500)
 ff = TF1("ff1", f, -1, 1, 1)
 ff.SetNpx(10000)
 ff.SetParameter(0, norm);
 ff.Draw()
 c.Draw()
 
-c = ROOT.TCanvas("myCanvasName","The Canvas Title", 1200, 500)
+c = ROOT.TCanvas("myfunc","The Canvas Title", 1200, 500)
 ff = TF1("ff1", f, -1, 1, 1)
 ff.SetNpx(10000)
 ff.SetParameter(0, norm)
 h1 = TH1F("h1", "fnneuman method", 500, -1, 1)
-for i in tqdm(range(0, 10000)):
-    h1.Fill(fnneuman(norm, a, b))
+for i in range(0, 10000):
+    h1.Fill(neuman(norm, a, b))
 h1.Draw()
 h1.Fit(ff)
 c.Draw()
 
-c = ROOT.TCanvas("myCanvasName2","The Canvas Title", 1200, 500)
+c = ROOT.TCanvas("myfunc","The Canvas Title", 1200, 500)
 ff2 = TF1("ff2", f, -1, 1, 1)
 ff2.SetNpx(10000)
 ff2.SetParameter(0, norm)
-h2 = TH1F("h2", "Second method", 500, -1, 1)
+h2 = TH1F("h2", "Method suchestv viborki", 500, -1, 1)
 
-for i in tqdm(range(0, 10000)):
-    h2.Fill(method2())
+for i in range(0, 10000):
+    h2.Fill(suchestv_viborka())
 h2.Draw()
 h2.Fit(ff2)
 c.Draw()
